4ba5863e7a2ef0fb8df6826c346e5c766d14e39c
Roniel Contreras <ronielcontreras70@gmail.com>
2024-12-30 17:34:43 -0500

    Add app.ts pinia store

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,8 +1,55 @@
 // Utilities
+import Session from '@/models/Session'
+import router from '@/router'
 import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
-  state: () => ({
-    //
-  }),
+  state: () => {
+    return {
+      session: new Session() as Session
+    }
+  },
+
+  actions: {
+    setSession(session: Session) {
+      localStorage.removeItem(Session.KEY)
+      this.session = session
+      this.saveSession()
+    },
+
+    saveSession() {
+      localStorage.setItem(Session.KEY, JSON.stringify(this.session))
+    },
+
+    loadSession() {
+      let cachedSession = localStorage.getItem(Session.KEY)
+      if (cachedSession != null) {
+        this.session = JSON.parse(cachedSession)
+      }
+    },
+
+    destroySession() {
+      this.session = new Session()
+      this.saveSession()
+    },
+
+    async checkSession() {      
+      this.loadSession()
+
+      if (this.session.token == undefined) {
+        router.push("/auth/login")
+        return
+      }
+
+      try {
+        //Call api do check session and store it in this variable
+        let session: Session = new Session()
+        console.log(session)
+        this.setSession(session)
+      } catch (e) {
+        this.destroySession()
+        router.push("/auth/login")
+      }
+    },
+  }
 })
